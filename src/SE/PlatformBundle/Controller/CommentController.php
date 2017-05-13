d31fd1294574de879867da242736efe285468ff6
Bachir TADJER <tadjer.bachir@gmail.com>
2017-05-13 17:58:23 +0200

    Comment Filter
    Filter comment for user by Advert

---
+++ src/SE/PlatformBundle/Controller/CommentController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace SE\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CommentController extends Controller
+{
+    /* PRIVATE VAR */
+    private $em;
+    private $advert;
+
+    /* PRIVATE FUNCTION */
+    private function getDoctrineManager(){
+      return $this->getDoctrine()->getManager();
+    }
+
+    private function getListUserFilterAttributes(Request $request){
+        $this->advert = $request->query->get('advert');
+    }
+
+    private function getAdvertByUser(){
+      $em = $this->getDoctrineManager();
+      $user;
+      //return $em->getRepository('SEPlatformBundle:Advert')->findByUser($user);
+
+      $list = array(
+        array('id'=>1, 'title'=>'Mon annonce 1', 'state'=>1),
+        array('id'=>2, 'title'=>'Mon annonce 2', 'state'=>1),
+        array('id'=>3, 'title'=>'Mon annonce 3', 'state'=>2)
+      );
+      return $list;
+    }
+
+    private function getAdvertByComment(){
+      $em = $this->getDoctrineManager();
+      $user;
+      //return $em->getRepository('SEPlatformBundle:Advert')->findByUser($user);
+
+      $list = array(
+        array('id'=>1, 'title'=>'Mon annonce 1', 'state'=>1),
+        array('id'=>2, 'title'=>'Mon annonce 2', 'state'=>1),
+        array('id'=>3, 'title'=>'Mon annonce 3', 'state'=>2)
+      );
+      return $list;
+    }
+    /* PUBLIC FUNCTION */
+
+//Admin
+    public function addAction(Request $request){
+    }
+
+//Admin
+    public function editAction($id){
+
+    }
+
+//Admin
+    public function deleteAction($id){
+
+    }
+
+//Admin
+    public function viewAction($slug, $id)
+    {
+
+    }
+
+//Super ADMIN
+    public function listAction(Request $request)
+    {
+
+    }
+
+//Admin
+    public function listUserAction(Request $request)
+    {
+      return $this->render('SEPlatformBundle:Comment:listUser.html.twig',
+      array(
+        'advert'=> $this->advert,
+        'listAdvertUser'=>$this->getAdvertByUser(),
+        'listAdvertComment'=>$this->getAdvertByComment()
+      ));
+    }
+}
