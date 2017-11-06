0838b11b33dd9f190a6fd860e108711409a500bb
Bachir TADJER <tadjer.bachir@gmail.com>
2017-11-06 11:58:09 +0100

    Refe SEO
    Add Meta balise

--- livraison/sql/request.sql
+++ livraison/sql/request.sql
@@ -1,3 +1,9 @@
+
+--Non actif
 SELECT username, email,concat("/link/", confirmation_token) as link
 FROM `user`
 where enabled = 0
+
+--Non complet
+SELECT concat(email, ',') FROM `user`
+where isAcountComplete = 0 and enabled = 1
